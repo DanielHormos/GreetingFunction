e461750859ceb64013015809890433a785f64b20
Daniel Hormos <daniel_danne32@hotmail.com>
2024-09-06 16:16:15 +0200

    Ändring till greeting funktion med nedräkning av dagar

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const prompt = require('prompt-sync')();
 
-function greet(namePassedIn) {
-  return 'Welcome to SALT, ';
+function greet(namePassedIn, dateOfCourseStart) {
+  var today = new Date();
+  var dateToStart = new Date(dateOfCourseStart);
+  console.log(today.getDate());
+  var days = dateToStart.getDate() - today.getDate();
+  return 'Welcome to SALT, ' + namePassedIn  +  '\nToday it is ' + days + ' days left until the course starts';
 }
 
 // Main program
 const name = prompt('What is your name? ');
-const greeting = greet(name);
+const dateOfCourseStart = prompt('When does the course start? ')
+const greeting = greet(name, dateOfCourseStart);
 console.log(greeting);
 
 module.exports.greet = greet;
